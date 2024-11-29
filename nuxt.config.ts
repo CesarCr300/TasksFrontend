15ff110bd6fad5a9f81bb81c15fec28c43f775a3
Cesar <cesarjcontreras30@gmail.com>
2024-11-28 22:29:52 -0500

    fix(task.service): modify the way useRuntimeConfig is used to avoid the Nuxt instance unavailable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  app:{
-    head:{
-      title: "My TODO App"
-    }
+  app: {
+    head: {
+      title: "My TODO App",
+    },
   },
-  runtimeConfig:{
-    public:{
-      apiBaseURL: "http://localhost:4000"
-    }
-  }
-})
+  runtimeConfig: {
+    public: {
+      apiBaseURL: "http://localhost:4000",
+    },
+  },
+});
--- services/task.service.ts
+++ services/task.service.ts
@@ -1,40 +1,42 @@
-import { useRuntimeConfig } from "#app";
+export const useTaskService = () => {
+  const config = useRuntimeConfig();
+  const API_BASE = config.public.apiBaseURL;
 
-const config = useRuntimeConfig();
-
-const API_BASE = config.public.apiBaseUrl;
-
-class TaskService {
-  static async get() {
+  const get = async () => {
     const response = await $fetch(`${API_BASE}/tasks`);
     return response;
-  }
+  };
 
-  static async create(task: { title: string; description: string }) {
+  const create = async (task: { title: string; description: string }) => {
     const response = await $fetch(`${API_BASE}/tasks`, {
       method: "POST",
       body: task,
     });
     return response;
-  }
+  };
 
-  static async update(
+  const update = async (
     id: number,
     updates: { title: string; description: string }
-  ) {
+  ) => {
     const response = await $fetch(`${API_BASE}/tasks/${id}`, {
       method: "PUT",
       body: updates,
     });
     return response;
-  }
+  };
 
-  static async delete(id: number) {
+  const remove = async (id: number) => {
     const response = await $fetch(`${API_BASE}/tasks/${id}`, {
       method: "DELETE",
     });
     return response;
-  }
-}
+  };
 
-export default TaskService;
+  return {
+    get,
+    create,
+    update,
+    remove
+  };
+};
