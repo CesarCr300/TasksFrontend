c00b633218943a90bc2d714295f822fd00d82afb
Cesar <cesarjcontreras30@gmail.com>
2024-11-29 00:20:08 -0500

    feat(tasks): render tasks by status

---
+++ components/TaskList.vue
@@ -0,0 +1,21 @@
+<template class="task-list">
+    <h2 class="task-list__title">{{ title }}</h2>
+    <ul>
+        <li v-for="task in taskFiltered" :key="task.id">
+            <h3>{{ task.title }}</h3>
+            <p>{{ task.description }}</p>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: String,
+    tasks: Array,
+    status: String
+})
+
+const taskFiltered = computed(()=>{
+    return props.tasks.filter(task=>task.status === props.status)
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-    <div>Base</div>
+    <div>TODO</div>
+    <TaskList title="Pendiente" :tasks="tasks" status="pending"/>
+    <TaskList title="En progreso" :tasks="tasks" status="in-progress"/>
+    <TaskList title="Completado" :tasks="tasks" status="completed"/>
 </template>
+
+<script setup lang="ts">
+import {useTaskStore} from "~/stores/task.store"
+const store = useTaskStore();
+
+const tasks = computed(()=>store.tasks)
+
+onMounted(()=>{
+    store.fetchTasks();
+})
+</script>
